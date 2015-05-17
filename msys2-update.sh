c75382f5b829e4fa19321179a364df4c5b794db6
Ken-ichiro SHIMOJYO <ken@smj.to>
2015-05-17 17:11:43 +0900

    Added ssh setting and ruby gem to msys-update.sh.

--- msys2-update.sh
+++ msys2-update.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-pacman -Syu
-pacman -S base
-pacman -S base-devel
-pacman -S mingw-w64-x86_64
-pacman -S git subversion mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby mingw-w64-x86_64-pkgconf
+pacman -Syu --noconfirm
+pacman -S --noconfirm base
+pacman -S --noconfirm base-devel
+pacman -S --noconfirm mingw-w64-x86_64
+pacman -S --noconfirm git subversion mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby mingw-w64-x86_64-pkgconf lftp
+echo "C:\Users          /home" >> /etc/fstab
+gem update --system
+gem install gemcutter
+gem install ruby_gntp
+gem install rubyzip
+gem install RubyInline
+gem install ocra
