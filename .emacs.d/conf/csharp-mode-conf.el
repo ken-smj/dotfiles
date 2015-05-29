54aa97b0ee53f75db531f8ad6bf5dd114958eb91
k.shimojo <k.shimojo@psp.co.jp>
2015-05-29 22:43:52 +0900

    Added csharp-mode on plugins.

--- .emacs.d/conf/csharp-mode-conf.el
+++ .emacs.d/conf/csharp-mode-conf.el
@@ -5,4 +5,8 @@
 (require 'csharp-mode)
 (setq auto-mode-alist
       (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+(defun my-csharp-mode-hook ()
+  ;; enable the stuff you want for C# here
+  (electric-pair-mode 1))
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
 ;; ------------------------------------------------------------------------
