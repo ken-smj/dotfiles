8245b602bcec15cc5f6783a444af94e1cc301d9b
k.shimojo <k.shimojo@psp.co.jp>
2015-11-06 14:46:08 +0900

    changed msys2 update script.

--- msys2-update.sh
+++ msys2-update.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
+echo "C:\Users          /home" >> /etc/fstab
 pacman -Syu --noconfirm
 pacman -S --noconfirm base
 pacman -S --noconfirm base-devel
 pacman -S --noconfirm mingw-w64-x86_64
-pacman -S --noconfirm git subversion mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby mingw-w64-x86_64-pkgconf lftp
-echo "C:\Users          /home" >> /etc/fstab
+pacman -S --noconfirm git subversion mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby lftp
 gem update --system
 gem install gemcutter
 gem install ruby_gntp
