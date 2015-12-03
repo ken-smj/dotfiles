e19c8ab39e723f0abb256fae00fc84ed15cf27b4
Ken-ichiro SHIMOJYO <ken@smj.to>
2015-12-03 20:47:43 +0900

    make msys2 initializing script.

--- msys2-update.sh
+++ msys2-update.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
-echo "C:\Users          /home" >> /etc/fstab
-pacman -Syu --noconfirm
-pacman -S --noconfirm base
-pacman -S --noconfirm base-devel
-pacman -S --noconfirm mingw-w64-x86_64
-pacman -S --noconfirm git subversion mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby lftp mingw-w64-x86_64-ag
-gem update --system
-gem install gemcutter
-gem install ruby_gntp
-gem install rubyzip
-gem install RubyInline
-gem install ocra
+# msys2 setup script.
+# auther ken@smj.to
+
+home="C:\\Users"
+modules="base base-devel mingw-w64-x86_64 mingw-w64-x86_64-toolchain development git subversion \
+       mingw-w64-x86_64-imagemagick mingw-w64-x86_64-ruby lftp mingw-w64-x86_64-ag"
+gems="gemcutter ruby_gntp rubyzip RubyInline ocra oga"
+
+echo "This is msys2 setup scripts. version 2015.12.03."
+echo "Do you want to initialize? [yes / no]"
+read answer
+case "$answer" in
+	yes)
+	    pacman -Syu --noconfirm
+	    for i in $modules; do
+		pacman -S --noconfirm $i
+	    done
+	    ;;
+	*)
+	    echo "initializing was canceled."
+esac
+echo "checking mount $home on /home ..."
+mount=`cat /etc/fstab | grep /home | sed -e 's/^\([^#]\+\)\s\+\/home/\1/'`
+if [ -n "$mount" ]; then
+    echo "mount $mount on /home."
+else
+    echo "/home dose not set. do you wont set $home on /home ? [yes / no]"
+    read answer
+    case "$answer" in
+	yes)
+	    echo "$home          /home" >> /etc/fstab
+	    ;;
+	*)
+	    ;;
+    esac
+fi
+echo "Do you want to initialize Ruby gems? [yes / no]"
+read answer
+case "$answer" in
+	yes)
+	    gem update --system
+	    for i in $gems; do
+		gem install $i
+	    done
+	    ;;
+esac
+echo "Finished."
