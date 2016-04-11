b27a725736b43db40b38700cd5ea9800f9d29298
k.shimojo <k.shimojo@psp.co.jp>
2016-04-11 11:44:56 +0900

    modified mklink.bat.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -211,3 +211,34 @@
 ;;;
 ;;; end of file
 ;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-arguments (list "--smart-case" "--nogroup" "--column" "--"))
+ '(ag-executable "pt")
+ '(ag-highlight-search t)
+ '(ag-ignore-list
+   (list "/Win32/.+" "/x64/.+" "/bin.*/.+" "GRTAGS$" "GPATH$" "GTAGS$" "\\.xls.*$" "\\.ilk$" "\\.ncb$" "\\.pch$" "\\.pdb$" "\\.aps$" "\\.map$"))
+ '(ag-reuse-buffers (quote nil))
+ '(ag-reuse-window (quote nil))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(vc-handled-backends nil)
+ '(yas-trigger-key (kbd "<backtab>")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "black" :foreground "light gray"))))
+ '(cursor ((((class color) (background dark)) (:background "yellow")) (((class color) (background light)) (:background "yellow")) (t nil)))
+ '(org-date ((t (:foreground "orange" :underline t))))
+ '(outline-1 ((t (:foreground "orchid"))))
+ '(outline-2 ((t (:foreground "deep sky blue"))))
+ '(outline-3 ((t (:foreground "medium spring green"))))
+ '(outline-4 ((t (:foreground "pale goldenrod"))))
+ '(outline-5 ((t (:foreground "light slate blue"))))
+ '(outline-6 ((t (:foreground "light goldenrod"))))
+ '(outline-7 ((t (:foreground "tomato"))))
+ '(outline-8 ((t (:foreground "forest green")))))
