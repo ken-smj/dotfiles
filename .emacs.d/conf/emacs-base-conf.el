0f50b23f7aef5dad1b35097562020341bcfb0dc1
ken <ken@smj.to>
2016-05-06 12:54:22 +0900

    added osx's meta key settings.

--- .emacs.d/conf/emacs-base-conf.el
+++ .emacs.d/conf/emacs-base-conf.el
@@ -37,6 +37,14 @@
        '(("\\$changelog\\$\\.txt" . change-log-mode))
 	 auto-mode-alist))
 
+;; for osx
+(when (eq system-type 'darwin)
+  (setq mac-option-key-is-meta t)
+  (setq mac-command-key-is-meta nil)
+  (setq mac-command-modifier nil)
+  (setq mac-option-modifier 'meta)
+  )
+
 ;; for conf
 (require 'generic-x)
 (custom-set-variables
