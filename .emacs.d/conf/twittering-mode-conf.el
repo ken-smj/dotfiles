81b94b69f620c18b4b363fa11bb1c5495725b85a
k.shimojo <k.shimojo@psp.co.jp>
2016-05-20 21:18:03 +0900

    chnged twittering-mode setting.

--- .emacs.d/conf/twittering-mode-conf.el
+++ .emacs.d/conf/twittering-mode-conf.el
@@ -8,28 +8,39 @@
 ;; 認証済みaccess tokenをGnuPGで暗号化して保存する
 (setq twittering-use-master-password t)
 (setq twittering-use-ssl t)
-
 ;; アイコン表示
-(setq twittering-icon-mode t)
-
+(setq twittering-icon-mode nil)
 ;; タイムラインを5分(300秒)間隔で更新
 (setq twittering-timer-interval 300) 
-
-;; ;; アイコン取得時の情報表示をデフォルトで抑制するか
-;; (setq twittering-url-show-status t)  
-
-;; ;; 全てのアイコンを保存するか
-;; (setq twittering-icon-storage-limit t)
-
-;; ;; 最初に開くタイムラインを設定する
-;; (setq twittering-initial-timeline-spec-string
-;;       '(":replies"
-;;         ":favorites"
-;;         ":retweets_of_me"
-;;         ":home"
-;; ))
-;; ;; 短縮URLにbit.lyを使う
-;; (setq twittering-tinyurl-service 'bit.ly)
-;; (setq twittering-bitly-login "hogehoge")
-;; (setq twittering-bitly-api-key "hoge_key")
+;; アイコン取得時の情報表示をデフォルトで抑制するか
+(setq twittering-url-show-status t)  
+;; 全てのアイコンを保存するか
+(setq twittering-icon-storage-limit t)
+;; 最初に開くタイムラインを設定する
+(setq twittering-initial-timeline-spec-string
+      '(":home"
+	;; ":mentions"
+        ;; ":favorites"
+        ;; ":retweets_of_me"
+	))
+;; ツイート取得数
+(setq twittering-number-of-tweets-on-retrieval 50)
+;; 表示する書式
+(setq twittering-status-format "%RT{%FACE[bold]{RT}}%i %S [%s],  %@:\n%FOLD[  ]{%T // from %f%L%r%R%QT{\n+----\n%FOLD[|]{%i %s,  %@:\n%FOLD[  ]{%T // from %f%L%r%R}}\n+----}}\n ")
+;; ;; o で次のURLをブラウザでオープン
+;; (add-hook 'twittering-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "o")
+;;                (lambda ()
+;;                  (interactive)
+;;                  (twittering-goto-next-uri)
+;;                  (execute-kbd-macro (kbd "C-m"))
+;;                  ))))
+(defun my-eww-goto-url-new-session ()
+  (interactive)
+  (let ((uri (get-text-property (point) 'uri)))
+    (if uri
+	(eww uri))))
+(define-key twittering-mode-map (kbd "C-m")
+  'my-eww-goto-url-new-session)
 ;; ------------------------------------------------------------------------
