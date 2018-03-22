e797c3af75952c2e2117fcda907b2a740f0b37e8
下條 賢一郎 <k.shimojo@psp.co.jp>
2018-03-22 10:41:13 +0900

    disabled magit.

--- .emacs.d/conf/magit-conf.el
+++ .emacs.d/conf/magit-conf.el
@@ -2,9 +2,11 @@
 
 ;; ------------------------------------------------------------------------
 ;; @ magit
+(when (eq system-type 'windows-nt)
+  (custom-set-variables '(magit-git-executable "~/AppData/Local/Programs/Git/bin/git.exe")))
 (require 'magit)
-(with-eval-after-load 'info
-  (info-initialize)
-  (add-to-list 'Info-directory-list
-               "~/.emacs.d/site-lisp/magit/Documentation/"))
+;; (with-eval-after-load 'info
+;;   (info-initialize)
+;;   (add-to-list 'Info-directory-list
+;;                "~/.emacs.d/site-lisp/magit/Documentation/"))
 ;; ------------------------------------------------------------------------
