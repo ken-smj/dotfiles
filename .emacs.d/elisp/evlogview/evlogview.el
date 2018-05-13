4de31b565a82c21692eef7a3ba7a18b92b6be395
Ken-ichiro SHIMOJYO <ken@smj.to>
2018-05-13 22:30:15 +0900

    logview設定の実験。

--- .emacs.d/elisp/evlogview/evlogview.el
+++ .emacs.d/elisp/evlogview/evlogview.el
@@ -1,4 +1,4 @@
-;;; evlogview.el --- Major mode for viewing EVInsite's log files  -*- coding: utf-8 lexical-binding: t -*-
+;;; evlogview.el --- Major mode for viewing EVInsite's log files  -*- coding: utf-8 ; lexical-binding: t -*-
 
 ;; Copyright (C) 2018 Ken-ichiro Shimojyo
 
@@ -36,107 +36,22 @@
 ;;; Code:
 
 (require 'logview)
+(custom-set-variables
+ '(logview-additional-level-mappings
+   (quote
+    (("EVLOGGER"
+      (performance "PERFORM")
+      (debug       "DEBUG")
+      (message     "MESSAGE")
+      (warning     "WARNING")
+      (error      "ERROR")))))
+ '(logview-additional-submodes
+   (quote
+    (("EVLOGGER"
+      (format  "TIMESTAMP LEVEL PID[THREAD] -")
+      (levels . "EVLOGGER"))))))
 
-(setq major-mode 'evlogview-mode)
-(setq mode-name "EVLogView")
+;; (define-derived-mode evlogview logview "EVLogView"
+;;   "EVLogView mode is the extensions of logview-mode.")
+;;   ;; (define-key evlogview-mode-map "M-n" 'evlogview-next-far-time-stamp))
 
-;;; The mode.
-
-(defvar evlogview-mode-map
-  (let ((map (make-sparse-keymap)))
-    (suppress-keymap map)
-    (dolist (binding '(;; Movement commands.
-                       ("TAB" logview-go-to-message-beginning)
-                       ("n"   logview-next-entry)
-                       ("p"   logview-previous-entry)
-                       ("N"   logview-next-as-important-entry)
-                       ("P"   logview-previous-as-important-entry)
-                       ("M-n" logview-next-navigation-view-entry)
-                       ("M-p" logview-previous-navigation-view-entry)
-                       ("<"   logview-first-entry)
-                       (">"   logview-last-entry)
-                       ;; Narrowing/widening commands.
-                       ("["   logview-narrow-from-this-entry)
-                       ("]"   logview-narrow-up-to-this-entry)
-                       ("w"   widen)
-                       ("{"   logview-widen-upwards)
-                       ("}"   logview-widen-downwards)
-                       ;; Filtering by level commands.
-                       ("l 1" logview-show-only-errors)
-                       ("l e" logview-show-only-errors)
-                       ("l 2" logview-show-errors-and-warnings)
-                       ("l w" logview-show-errors-and-warnings)
-                       ("l 3" logview-show-errors-warnings-and-information)
-                       ("l i" logview-show-errors-warnings-and-information)
-                       ("l 4" logview-show-errors-warnings-information-and-debug)
-                       ("l d" logview-show-errors-warnings-information-and-debug)
-                       ("l 5" logview-show-all-levels)
-                       ("l t" logview-show-all-levels)
-                       ("+"   logview-show-only-as-important)
-                       ("l +" logview-show-only-as-important)
-                       ("L 1" logview-always-show-errors)
-                       ("L e" logview-always-show-errors)
-                       ("L 2" logview-always-show-errors-and-warnings)
-                       ("L w" logview-always-show-errors-and-warnings)
-                       ("L 3" logview-always-show-errors-warnings-and-information)
-                       ("L i" logview-always-show-errors-warnings-and-information)
-                       ("L 4" logview-always-show-errors-warnings-information-and-debug)
-                       ("L d" logview-always-show-errors-warnings-information-and-debug)
-                       ("L 0" logview-disable-unconditional-show)
-                       ("L L" logview-disable-unconditional-show)
-                       ;; Filtering by name/thread/message commands.
-                       ("f"   logview-edit-filters)
-                       ("a"   logview-add-include-name-filter)
-                       ("A"   logview-add-exclude-name-filter)
-                       ("t"   logview-add-include-thread-filter)
-                       ("T"   logview-add-exclude-thread-filter)
-                       ("m"   logview-add-include-message-filter)
-                       ("M"   logview-add-exclude-message-filter)
-                       ;; Filter resetting commands.
-                       ("r l" logview-reset-level-filters)
-                       ("r a" logview-reset-name-filters)
-                       ("r t" logview-reset-thread-filters)
-                       ("r m" logview-reset-message-filters)
-                       ("R"   logview-reset-all-filters)
-                       ("r e" logview-reset-all-filters-restrictions-and-hidings)
-                       ;; View commands.
-                       ("v"   logview-switch-to-view)
-                       ("V n" logview-set-navigation-view)
-                       ("V s" logview-save-filters-as-view-for-submode)
-                       ("V S" logview-save-filters-as-global-view)
-                       ("V e" logview-edit-submode-views)
-                       ("V E" logview-edit-all-views)
-                       ("V d" logview-delete-view)
-                       ;; Explicit entry hiding/showing commands.
-                       ("h"   logview-hide-entry)
-                       ("H"   logview-hide-region-entries)
-                       ("s"   logview-show-entries)
-                       ("S"   logview-show-region-entries)
-                       ;; Showing/hiding entry details commands.
-                       ("d"   logview-toggle-entry-details)
-                       ("D"   logview-toggle-region-entry-details)
-                       ("e"   logview-toggle-details-globally)
-                       ;; Option changing commands.
-                       ("o r" auto-revert-mode)
-                       ("o t" auto-revert-tail-mode)
-                       ("o v" logview-toggle-copy-visible-text-only)
-                       ("o m" logview-toggle-search-only-in-messages)
-                       ("o e" logview-toggle-show-ellipses)
-                       ("o s" logview-choose-submode)
-                       ("o S" logview-customize-submode-options)
-                       ;; For compatibility with the inactive keymap.
-                       ("C-c C-c" logview-choose-submode)
-                       ("C-c C-s" logview-customize-submode-options)
-                       ;; Miscellaneous commands.
-                       ("SPC" logview-pulse-current-entry)
-                       ("?"   logview-mode-help)
-                       ("g"   logview-refresh-buffer-as-needed)
-                       ("x"   logview-append-log-file-tail)
-                       ("X"   logview-revert-buffer)
-                       ("q"   bury-buffer)
-                       ;; Simplified universal argument command
-                       ;; rebindings.  Digits and minus are set up by
-                       ;; 'suppress-keymap' already.
-                       ("u"   universal-argument)))
-      (define-key map (kbd (car binding)) (cadr binding)))
-    map))
